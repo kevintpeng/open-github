157a8a5a4dbc3f4c5f9542f4dd9f5ccd707e26a8
Kevin Peng <kpeng@riotgames.com>
2018-05-14 21:26:57 -0700

    add unit tests

--- test/test_open_git.rb
+++ test/test_open_git.rb
@@ -91,4 +91,18 @@ class TestOpenGit < Minitest::Test
       end
     end
   end
+
+  def test_git_url
+    OpenGit::Git.stub(:remote_url, "https://github.com/kevintpeng/open_git.git") do
+      assert_equal "https://github.com/kevintpeng/open_git.git", OpenGit::Git.url("my_remote")
+    end
+  end
+
+  def test_git_url_invalid
+    assert_raises OpenGit::Git::InvalidRemoteError do
+      OpenGit::Git.stub(:remote_url, "my_remote") do
+        OpenGit::Git.url("my_remote")
+      end
+    end
+  end
 end
